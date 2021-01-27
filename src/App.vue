4a431e8bb3524ce8c86dd4b9a6bd6843d3b7c170
surayutttttttt <surayut.b@ku.th>
2021-01-27 16:49:57 +0700

    routes add from dog

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <div id="nav">
+      <router-link to="/">MyAssets</router-link> |
+      <router-link to="/">Add Assets</router-link> |
+      <router-link to="/">Assets Details </router-link> |
+      <router-link to="/">Report Problems </router-link> |
+      <router-link to="/">Edit Forms </router-link> |
+      <router-link to="/">Yoklerk</router-link> |
+
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from "./components/HelloWorld";
 
 export default {
   name: "App",
+
   components: {
     HelloWorld
-  }
+  },
+
+  data: () => ({
+    //
+  })
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/views/ReportProblems.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>ReportProblems</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
